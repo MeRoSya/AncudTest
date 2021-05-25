b06d92908ab3e606210f57359b06fb8d445c4050
Ivan Zhuginsky <Zhuga200117@gmail.com>
2021-05-25 17:05:59 +0300

    Убраны не используемые функции + добавлена возможность указывать выходной файл

--- ParallelSort.cpp
+++ ParallelSort.cpp
@@ -13,50 +13,6 @@ mutex mut;
 
 /*Functions' prototypes*/
 
-/* 
-Function break vector into t_num parts and sorts them
-in different threads. Then parts are merged. The algorythm
-is fast, but uses extra memory for merging, so it may have terrible
-efficiency by memory, while sorting large arrays
-
-Args:
-Values - reference to vector of unsigned ints to be sorted
-t_num - number of threads to be used
-*/
-void Threaded_Sort(vector<unsigned int> &Values, int t_num);
-
-/*
-Function, running by one thread, while sorting.
-Threads in that case have no shared memory, so
-there is no need for synchronization.
-
-Args:
-Values - reference to vector of unsigned ints to be sorted
-begin - first border
-end - second border
-*/
-void Threaded_It(vector<unsigned int> &Values, int begin, int end);
-
-/*
-Function break in halfs and then fork one thread to call recursion
-for the first half and call recursion for another half in this thread.
-After they finished, sorted parts are merged.
-Recursion continues, until either number of active threads not greater, than
-system core num. When recursion ends, current range of vector sorts by std::sort
-and then go on upper level of recursion, where sorted parts are merged.
-It is about 1.5 times faster, than Threaded_Sort 
-
-Comparasing for 1 GB file:
-Rec_Threaded_Sort: 36648462 mcs
-Threaded_Sort: 56514361 mcs
-
-Args:
-Values - reference to vector of unsigned ints to be sorted
-begin - first border
-end - second border
-*/
-void Rec_Threaded_Sort(vector<unsigned int> &Values, int begin, int end);
-
 /*
 Making merge sort, using files. It is not really fast (Sorting of file with
 size of 100 MB is 24665 ms), but it uses fixed amount of RAM 
@@ -65,7 +21,7 @@ for other variables), so it can be used to sort really big files
 Args:
 path - path to file to sort
 */
-void File_Sort(string path);
+void File_Sort(string input_path, string output_path);
 
 /*
 Function splits initial file into many files with size of 4 kb (standart claster size),
@@ -109,17 +65,19 @@ int main(int argc, char *argv[])
     and intialize it with default value
     */
 
-    string path = "Array";
+    string input_path = "./Array";
+    string output_path = "./Sorted_Array";
 
     /*Cmd arguments handling*/
     while (true)
     {
 
         static struct option long_options[] = {{"help", no_argument, 0, 'h'},
-                                               {"file", required_argument, 0, 'f'}};
+                                               {"ifile", required_argument, 0, 'i'},
+                                               {"ofile" , required_argument, 0, 'o'}};
         int option_index = 0;
 
-        int cur_opt = getopt_long(argc, argv, "hf:", long_options, &option_index);
+        int cur_opt = getopt_long(argc, argv, "hi:o:", long_options, &option_index);
 
         if (cur_opt == -1)
             break;
@@ -131,14 +89,21 @@ int main(int argc, char *argv[])
         case 'h':
             cout << "Options list:" << endl;
             cout << "-h [ --help ]\t\tShows help" << endl;
-            cout << "-f [ --file ] arg\tInput path to file with data" << endl;
-            cout << "if -f flag isn't used, program will try to use default file ./Array" << endl;
+            cout << "-if [ --ifile ] arg\tInput path to the file with data" << endl;
+            cout << "if -if flag isn't used, program will try to use default file ./Array" << endl;
+            cout << endl;
+            cout << "-of [ --ofile ] arg\tPath to the output file" << endl;
+            cout << "if -of flag isn't used, program will try to use default file ./Sorted_Array" << endl;
             return 0;
             break;
 
         /*Setting path to file*/
-        case 'f':
-            path = optarg;
+        case 'i':
+            input_path = optarg;
+            break;
+        
+        case 'o':
+            output_path = optarg;
             break;
 
         case '?':
@@ -152,72 +117,14 @@ int main(int argc, char *argv[])
     }
 
     cout << "Starting processes..." << endl;
-    File_Sort(path);
+    File_Sort(input_path, output_path);
 
     return 0;
 }
 
 /*Implementation of functions*/
 
-void Threaded_Sort(vector<unsigned int> &Values, int t_num)
-{
-    thread threads[t_num];
-    int nodes[t_num + 1];
-    nodes[0] = 0;
-
-    /*Cheking, if size of vector is lesser, than 2*t_num.
-    If it is so, then no need to start threads,
-    because parts will be of size 1 and it will not be
-    efficient*/
-    if (2 * t_num > Values.size())
-        sort(Values.begin(), Values.end());
-    else
-    {
-        /*Starting t_num threads to sort one part of vector each*/
-        for (int i = 0; i < t_num; i++)
-        {
-            nodes[i + 1] = (i + 1) * Values.size() / t_num;
-            threads[i] = thread(Threaded_It, ref(Values), nodes[i], nodes[i + 1]);
-        }
-
-        /*Join those threads*/
-        for (int i = 0; i < t_num; i++)
-        {
-            if (threads[i].joinable())
-                threads[i].join();
-        }
-
-        /*Merge sorted parts of vector*/
-        for (int i = 1; i < t_num; i++)
-        {
-            inplace_merge(Values.begin(), Values.begin() + nodes[i], Values.begin() + nodes[i + 1]);
-        }
-    }
-}
-
-void Threaded_It(vector<unsigned int> &Values, int begin, int end)
-{
-    sort(Values.begin() + begin, Values.begin() + end);
-}
-
-void Rec_Threaded_Sort(vector<unsigned int> &Values, int begin, int end)
-{
-    static atomic_int t_num(0);
-    t_num++;
-    if (!(t_num > sysconf(_SC_NPROCESSORS_ONLN) / 2) && (end > 128))
-    {
-        thread t(Rec_Threaded_Sort, ref(Values), 0, end / 2);
-        t.join();
-        Rec_Threaded_Sort(ref(Values), end / 2, end);
-        inplace_merge(Values.begin() + begin, Values.begin() + end / 2, Values.begin() + end);
-    }
-    else
-    {
-        sort(Values.begin() + begin, Values.begin() + end);
-    }
-}
-
-void File_Sort(string path)
+void File_Sort(string input_path, string output_path)
 {
     cout << "Reading from file..." << endl;
 
@@ -225,16 +132,16 @@ void File_Sort(string path)
     Splitting initial file into many smaller ones, 
     elements in which are sorted
     */
-    File_Split(path);
+    File_Split(input_path);
 
     /*Merging all sorted files*/
     Threaded_Files_Merge_Sort(1);
 
-    /*Copying sorted file from temporary dir to main dir*/
-    boost::filesystem::copy_file("0", "../Sorted_Array", boost::filesystem::copy_option::overwrite_if_exists);
-
     /*Going back to main dir*/
     boost::filesystem::current_path("..");
+
+    /*Copying sorted file from temporary dir to main dir*/
+    boost::filesystem::copy_file("./temp/0", output_path, boost::filesystem::copy_option::overwrite_if_exists);
 
     /*Removing temporary dir*/
     cout << "Removing temporary files..." << endl;
