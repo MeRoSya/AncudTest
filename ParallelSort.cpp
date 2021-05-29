5f52361cd1ed266fa1c0cc99deb6b1c8ebc7aec7
Ivan Zhuginsky <Zhuga200117@gmail.com>
2021-05-29 16:52:22 +0300

    Finished

--- ParallelSort.cpp
+++ ParallelSort.cpp
@@ -7,7 +7,6 @@
 #include <fcntl.h>
 #include <getopt.h>
 #include <cmath>
-#include <chrono>
 
 using namespace std;
 
@@ -24,12 +23,30 @@ public:
 class MappedFile : public IFile
 {
 
+    /*
+    Function, which sorts array of values using modified merge-sort.
+    Args:
+    ptr - Array
+    begin - Start point of sorting
+    end - End point of sorting
+    */
     static void Rec_Threaded_Sort(unsigned int *&ptr, int begin, int end);
 
 protected:
+
+    /*The pointer to the begin of the information in the file*/
     unsigned int *Array;
+
+    /*The size of the file (the number of elements)*/
     unsigned long long int size;
+
+    /*The file descriptor*/
+    int fd;
+
+    /*The path to the input file*/
     string input_path;
+
+    /*The path to the output file*/
     string output_path;
 
 public:
@@ -37,11 +54,23 @@ public:
     MappedFile(string input_path = "./Array", string output_path = "./Sorted_Array");
 
     /*Implementing interface's methods*/
-    virtual void Read();
-    void Sort();
-    virtual void Write();
 
-    /*Additional method to implement reading of the any file, not only input*/
+    /*Reads the input file*/
+    virtual void Read();
+
+    /*Sorts data in the output file*/
+    void Sort();
+
+    /*
+    Writes data to the output file
+    (copies the input file)
+    */
+    virtual void Write();
+    
+    /*
+    Additional method to implement reading of any file, not only the input one.
+    Sets the pointer Array to point on the begin of the info in the file path.
+    */
     void Read(string path);
 
     /*Destructor*/
@@ -79,11 +108,11 @@ int main(int argc, char *argv[])
         case 'h':
             cout << "Options list:" << endl;
             cout << "-h [ --help ]\t\tShows help" << endl;
-            cout << "-if [ --ifile ] arg\tA path to the input file" << endl;
-            cout << "if -if flag isn't used, program will try to use default file ./Array" << endl;
+            cout << "-i [ --ifile ] arg\tA path to the input file" << endl;
+            cout << "if -i flag isn't used, program will try to use default file ./Array" << endl;
             cout << endl;
-            cout << "-of [ --ofile ] arg\tA path to the output file" << endl;
-            cout << "if -of flag isn't used, program will try to use default file ./Sorted_Array" << endl;
+            cout << "-o [ --ofile ] arg\tA path to the output file" << endl;
+            cout << "if -o flag isn't used, program will try to use default file ./Sorted_Array" << endl;
             return 0;
             break;
 
@@ -130,7 +159,7 @@ MappedFile::MappedFile(string input_path, string output_path)
     }
     catch (boost::filesystem::filesystem_error ex)
     {
-        cout << "Error occurred: " << ex.what();
+        cout << "An error occurred: " << ex.what() << endl;
         exit(1);
     }
     this->Array = nullptr;
@@ -138,7 +167,7 @@ MappedFile::MappedFile(string input_path, string output_path)
 
 void MappedFile::Read(string path)
 {
-    int fd;
+    
     if ((fd = open(path.c_str(), O_RDWR)))
     {
         cout << "Reading the file " << path << endl;
@@ -147,7 +176,7 @@ void MappedFile::Read(string path)
     }
     else
     {
-        cout << "Error occurred, while trying to read the file" << endl;
+        cout << "An error occurred, while trying to read the file" << endl;
         exit(1);
     }
 }
@@ -159,20 +188,23 @@ void MappedFile::Read()
 
 void MappedFile::Write()
 {
+    /*Checking, if there are enough disk space available to create a copy of the initial file*/
     if (boost::filesystem::space(boost::filesystem::current_path()).available > size * sizeof(unsigned int))
         try
         {
+            /*Copy the input file to the output file*/
+            cout << "Reading the file " << input_path << endl;
             cout << "Writing to the file " << output_path << endl;
             boost::filesystem::copy(input_path, output_path, boost::filesystem::copy_options::overwrite_existing);
         }
         catch (boost::filesystem::filesystem_error ex)
         {
-            cout << "Error occurred, while trying to write the file: " << ex.what();
+            cout << "The error occurred, while trying to write the file: " << ex.what();
             exit(1);
         }
     else
     {
-        cout << "Error occurred: Not enough disk space to create the file's copy" << endl;
+        cout << "The error occurred: Not enough disk space to create the file's copy" << endl;
         exit(1);
     }
 }
@@ -180,6 +212,11 @@ void MappedFile::Write()
 void MappedFile::Sort()
 {
 
+    /*
+    Setting output file and copy data from input
+    file to it. If input and output are the same files
+    only read the input
+    */
     if (input_path != output_path)
     {
         this->Write();
@@ -187,19 +224,34 @@ void MappedFile::Sort()
     }
     else
         this->Read();
+    
+    /*Sorting the file*/
     cout << "Sorting the file " << output_path << endl;
     Rec_Threaded_Sort(ref(Array), 0, size);
+    cout << "Done" << endl;
 }
 
 void MappedFile::Rec_Threaded_Sort(unsigned int *&ptr, int begin, int end)
 {
+    /*Threads counter*/
     static atomic_int t_num(0);
     t_num++;
-    if (!(t_num > thread::hardware_concurrency() / 2) && (end > pow(2, 30) - 1))
+
+    /*
+    end-begin - part size
+    Optimal minimal part size is 2 Mb (read README.md for more information)
+    */
+    if (!(t_num > thread::hardware_concurrency()) && !(end-begin < pow(2, 21)/sizeof(unsigned int)))
     {
-        thread t(Rec_Threaded_Sort, ref(ptr), 0, end / 2);
-        t.join();
+        thread t(Rec_Threaded_Sort, ref(ptr), begin, end / 2); 
         Rec_Threaded_Sort(ref(ptr), end / 2, end);
+        t.join();
+
+        /*
+        Following the description of std::inplace_merge() it uses additional RAM ONLY,
+        if it is possible. So there can't be the situation, when it is used more memory,
+        than is available
+        */
         inplace_merge(ptr + begin, ptr + end / 2, ptr + end);
     }
     else
@@ -210,4 +262,5 @@ void MappedFile::Rec_Threaded_Sort(unsigned int *&ptr, int begin, int end)
 
 MappedFile::~MappedFile()
 {
+    int fc = close(fd);
 }
